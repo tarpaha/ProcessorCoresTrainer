c532f821213f3a8b5e7325974bb5bb86654fd361
tarpaha <ppopchikovsky@gmail.com>
2020-07-30 00:48:31 +0300

    utils project to calculate proper grid dimensions

---
+++ Utils/Dimensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class Dimensions
+    {
+        /// <summary>
+        /// Returns rectangle dimensions for nice alignment of passed cells 
+        /// </summary>
+        public static (int, int) Calculate(int cells)
+        {
+            var cols = (int)Math.Ceiling(Math.Sqrt(cells));
+            while (true)
+            {
+                var rows = (int)Math.Ceiling((double)cells / cols);
+                if (cols * rows == cells)
+                    return (cols, rows);
+                cols += 1;
+            }
+        }
+    }
+}
